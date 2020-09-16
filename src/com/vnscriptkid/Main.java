4e7b0272c4cedd796bc52b1c4c1699ddf1fff9ed
Thanh Nguyen <vnscriptkid@gmail.com>
2020-09-16 22:48:25 +0700

    Implement dijkstra algorithm

--- src/com/vnscriptkid/Main.java
+++ src/com/vnscriptkid/Main.java
@@ -361,7 +361,24 @@ public class Main {
         graph.print();
     }
 
+    public static void useDjikstra() {
+        WeightedGraphOOP graph = new WeightedGraphOOP();
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addNode("D");
+        graph.addNode("E");
+        graph.addEdge("A", "B", 3);
+        graph.addEdge("A", "D", 2);
+        graph.addEdge("A", "C", 4);
+        graph.addEdge("B", "D", 6);
+        graph.addEdge("C", "D", 1);
+        graph.addEdge("B", "E", 1);
+        graph.addEdge("E", "D", 5);
+        graph.dijkstra("A");
+    }
+
     public static void main(String[] args) {
-        useWeightedGraph();
+        useDjikstra();
     }
 }
--- src/com/vnscriptkid/WeightedGraphOOP.java
+++ src/com/vnscriptkid/WeightedGraphOOP.java
@@ -1,9 +1,15 @@
 package com.vnscriptkid;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
 
 public class WeightedGraphOOP {
     private class Node {
@@ -28,6 +34,12 @@ public class WeightedGraphOOP {
         public String toString() {
             return label;
         }
+
+        @Override
+        public boolean equals(Object obj) {
+            Node other = (Node) obj;
+            return this.label.equals(other.label);
+        }
     }
 
     private class Edge {
@@ -48,7 +60,6 @@ public class WeightedGraphOOP {
     }
 
     private Map<String, Node> nodes = new HashMap<>();
-    // private Map<Node, List<Edge>> adjacencyList = new HashMap<>();
 
     public void addNode(String label) {
         Node newNode = new Node(label);
@@ -63,8 +74,6 @@ public class WeightedGraphOOP {
             throw new IllegalArgumentException("Node does not exist");
         }
         
-        // adjacencyList.get(fromNode).add(new Edge(fromNode, toNode, weight));
-        // adjacencyList.get(toNode).add(new Edge(toNode, fromNode, weight));
         fromNode.addEdge(toNode, weight);
         toNode.addEdge(fromNode, weight);
     }
@@ -76,4 +85,70 @@ public class WeightedGraphOOP {
             }                        
         }
     }
-}
+
+    public void dijkstra(String fromNodeLabel) {
+        Node fromNode = nodes.get(fromNodeLabel);
+        if (fromNode == null)
+            throw new IllegalArgumentException();
+        class Entry {
+            Node toNode;
+            int distance;
+
+            public Entry(Node toNode, int distance) {
+                this.toNode = toNode;
+                this.distance = distance;
+            }
+
+            public Node getNode() {
+                return toNode;
+            }
+
+            public Integer getDistance() {
+                return distance;
+            }
+        }
+        
+        Set<Node> visited = new HashSet<>();
+        Map<Node, Integer> distances = new HashMap<>(); // <toNode, distance>
+        Map<Node, Node> prevNodes = new HashMap<>(); // <toNode, throughNode>
+
+        // init distances
+        for (Node node : nodes.values()) {
+            if (node.equals(fromNode)) {
+                distances.put(node, 0);
+            } else {
+                distances.put(node, Integer.MAX_VALUE);
+            }
+        }
+        Node currentNode = fromNode;
+        while (true) {
+            visited.add(currentNode);
+            PriorityQueue<Entry> queue = new PriorityQueue<>(
+                Comparator.comparingInt(Entry::getDistance)
+            );
+            for (Edge edge : currentNode.getEdges()) {
+                Node neighbor = edge.toNode;
+                if (visited.contains(neighbor))
+                    continue;
+                int directDistanceFromCurrentToNeighbor = edge.weight;
+                int shortestPathToCurrent = distances.get(currentNode);
+                int shortestPathToNeighbor = distances.get(neighbor);
+                int shortestCandidate = shortestPathToCurrent + directDistanceFromCurrentToNeighbor;
+                if (shortestCandidate < shortestPathToNeighbor) {
+                    distances.put(neighbor, shortestCandidate);
+                    prevNodes.put(neighbor, currentNode);
+                }
+                queue.add(new Entry(neighbor, distances.get(neighbor)));
+            }
+            if (queue.size() == 0)
+                break;
+            currentNode = queue.poll().getNode();
+            queue.clear();
+        }
+
+        // result
+        for (Node node : distances.keySet()) {
+            System.out.println(fromNode + " -> " + node + " = " + distances.get(node));
+        }
+    }
+}   
